d1de6f66ab443aac441ea715f122d8e27a63aebc
ankit <ankit@trakinvest.com>
2019-11-19 10:39:09 +0530

    changes to run a bot from java console

--- src/main/java/com/yellow/messenger/bot/BotApplication.java
+++ src/main/java/com/yellow/messenger/bot/BotApplication.java
@@ -1,13 +1,53 @@
 package com.yellow.messenger.bot;
 
+import com.yellow.messenger.bot.util.DetectIntentTexts;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class BotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BotApplication.class, args);
+
+		String userInput;
+
+		Scanner sn = new Scanner(System.in);
+
+
+		System.out.println("*. Lets Chat");
+		System.out.println("*. Type exit to exit");
+		while(true){
+			// Prompt the use to make a choice
+			System.out.println("User:");
+
+			//Capture the user input in scanner object and store it in a pre decalred variable
+			userInput = sn.nextLine();
+
+			//Check the user input
+			switch(userInput){
+				case "exit":
+					//exit from the program
+					System.out.println("Exiting...");
+					System.exit(0);
+				default:
+					//inform user in case of invalid choice.
+					List<String> test =new ArrayList<String>();
+					test.add(userInput);
+					try {
+						DetectIntentTexts.detectIntentTexts("yellowmessenger-iubnee",test,"sahvdhjavdhjashg","en");
+					} catch (Exception e) {
+						System.exit(0);
+					}
+			}
+		}
+
+
 	}
 
 }
--- src/test/java/com/yellow/messenger/bot/BotApplicationTests.java
+++ src/test/java/com/yellow/messenger/bot/BotApplicationTests.java
@@ -1,13 +1,26 @@
 package com.yellow.messenger.bot;
 
+import com.yellow.messenger.bot.util.DetectIntentTexts;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
 
 @SpringBootTest
 class BotApplicationTests {
 
 	@Test
 	void contextLoads() {
+		ArrayList<String> strings = new ArrayList<>();
+
+		strings.add("Hi");
+		strings.add("How are you doing");
+		try {
+			DetectIntentTexts.detectIntentTexts("yellowmessenger-iubnee",strings,"sahvdhjavdhjashg","en");
+		} catch (Exception e) {e.printStackTrace();
+
+		}
+
 	}
 
 }
